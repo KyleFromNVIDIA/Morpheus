1e78520aaaa7d1aa54f94c336d8952a1f9e11a07
David Gardner <96306125+dagardner-nv@users.noreply.github.com>
2025-09-03 08:08:24 -0700

    Misc version and CI fixes (#2306)
    * Set GPU runner to an earlier driver version
    * Document #2305 as a known issue
    * Pin `npy-append-array` to avoid version conflict with nvtabular
    * Update rapids shared workflows to 24.10 to fix the check action
    * Pin click version to <8.2 to avoid #2219
    * Ensure the logger is reset in `test_cli.py`
    
    ## By Submitting this PR I confirm:
    - I am familiar with the [Contributing Guidelines](https://github.com/nv-morpheus/Morpheus/blob/main/docs/source/developer_guide/contributing.md).
    - When the PR is ready for review, new or existing tests cover these changes.
    - When the PR is ready for review, the documentation is up to date with these changes.
    
    Authors:
      - David Gardner (https://github.com/dagardner-nv)
      - https://github.com/Salonijain27
    
    Approvers:
      - Will Killian (https://github.com/willkill07)
    
    URL: https://github.com/nv-morpheus/Morpheus/pull/2306

--- docs/source/extra_info/known_issues.md
+++ docs/source/extra_info/known_issues.md
@@ -1,5 +1,5 @@
 <!--
-SPDX-FileCopyrightText: Copyright (c) 2022-2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+SPDX-FileCopyrightText: Copyright (c) 2022-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 SPDX-License-Identifier: Apache-2.0
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,6 +17,7 @@ limitations under the License.
 
 # Known Issues
 
+- numba triggering IndexError on systems with driver version 580+ ([#2305](https://github.com/nv-morpheus/Morpheus/issues/2305))
 - TrainAEStage fails with a Segmentation fault ([#1641](https://github.com/nv-morpheus/Morpheus/issues/1641))
 - vdb_upload example pipeline triggers an internal error in Triton ([#1649](https://github.com/nv-morpheus/Morpheus/issues/1649))
 
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# SPDX-FileCopyrightText: Copyright (c) 2022-2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2022-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -125,7 +125,7 @@ def mlflow_uri(tmp_path):
 
     yield uri
 
-    num_runs = len(fluent._active_run_stack)
+    num_runs = len(fluent._active_run_stack.get())
     for _ in range(num_runs):
         mlflow.end_run()
 
@@ -140,7 +140,7 @@ def config_warning_fixture():
 
 
 @pytest.mark.reload_modules(commands)
-@pytest.mark.usefixtures("chdir_tmpdir", "reload_modules")
+@pytest.mark.usefixtures("chdir_tmpdir", "reload_modules", "reset_logging")
 @pytest.mark.use_python
 class TestCLI:
 
